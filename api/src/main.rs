5c729017f2a1bdae4ee6d661cb6dbd02c557db1a
Preston Harrison <preston@prestonharrison.com>
2022-10-25 19:59:36 +1000

    containerise everything under the sun

--- api/src/main.rs
+++ api/src/main.rs
@@ -2,7 +2,7 @@ use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    let port: u16 = std::env::var("PATH").unwrap().parse().unwrap();
-    let hello_world = warp::path!("/hello" / String).map(|name| format!("Hello, {}", name));
+    let port: u16 = std::env::var("PORT").unwrap().parse().unwrap();
+    let hello_world = warp::path!("hello" / String).map(|name| format!("Hello, {}", name));
     warp::serve(hello_world).run(([0, 0, 0, 0], port)).await
 }
