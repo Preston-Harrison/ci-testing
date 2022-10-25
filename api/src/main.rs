100c3bc04601db2c37f5f1a0655ab1dacc6d5c3c
Preston Harrison <preston@prestonharrison.com>
2022-10-25 19:11:09 +1000

    rust boilerplate

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let port: u16 = std::env::var("PATH").unwrap().parse().unwrap();
+    let hello_world = warp::path!("/hello" / String).map(|name| format!("Hello, {}", name));
+    warp::serve(hello_world).run(([0, 0, 0, 0], port)).await
 }
