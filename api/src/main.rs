a6d936bab94189c37d2080c72d17cbe0bb7fbd29
Preston Harrison <preston@prestonharrison.com>
2022-10-27 19:31:39 +1000

    remove compose down

--- api/src/main.rs
+++ api/src/main.rs
@@ -3,6 +3,6 @@ use warp::Filter;
 #[tokio::main]
 async fn main() {
     let port: u16 = std::env::var("PORT").unwrap().parse().unwrap();
-    let hello_world = warp::path!("hello" / String).map(|s| format!("Hello {}", s));
+    let hello_world = warp::path!(String / String).map(|s1, s2| format!("{} {}", s1, s2));
     warp::serve(hello_world).run(([0, 0, 0, 0], port)).await;
 }
